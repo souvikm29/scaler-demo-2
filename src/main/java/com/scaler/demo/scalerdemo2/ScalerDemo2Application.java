5da0f2bcb02d21df426d910035c083813da646d6
Souvik <souvikm29@gmail.com>
2024-05-07 19:12:38 +0530

    Initial Commit

---
+++ src/main/java/com/scaler/demo/scalerdemo2/ScalerDemo2Application.java
@@ -0,0 +1,13 @@
+package com.scaler.demo.scalerdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerDemo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/demo/scalerdemo2/ScalerDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.demo.scalerdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
